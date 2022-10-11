b79e8e66c64cd87b647ce565acfa6259bbbab5df
Michaela Vosvrdova <vosv@protonmail.com>
2022-10-11 09:10:58 +0200

    Úkol č. 3

--- ukol-03.py
+++ ukol-03.py
@@ -1,31 +1,10 @@
-# Zeptá se uživatele na číslo, kam chce zprávu zaslat a ověří, že číslo má správný formát.
-# Zeptá se uživatele na zprávu, kterou chce zaslat. Následně vypíše, kolik zpráva bude stát.
-# Tvůj program bude obsahovat dvě funkce:
-
-# První funkce ověří délku telefonního čísla. Uvažuj, že telefonní číslo může být devítimístné 
-# nebo třináctimístné (pokud je na začátku předvolba "+420"). Nemusíte kontrolovat, zda uživatel 
-# zadal skutečně číslo, či zda jsou tam i jiné znaky. To jsme v kurzu zatím neřešili. 
-# Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
-# Druhá funkce spočte cenu zprávy. Uživatel platí 3 Kč za každých započatých 180 znaků.
-# Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. 
-# Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce
-# určí její cenu, kterou vypíše uživateli.
-
 zadane_cislo = input('Zadej telefonní číslo příjemce: ')
 
 def over_delku(cislo):
-    str(cislo)
-    predvolba = str(+420)
+    predvolba = str('+420')
     if ((predvolba in cislo) and (len(cislo) == 13)) or (len(cislo) == 9):
         return True
     return False
-
-over_delku(zadane_cislo)
-
-if over_delku:
-    text = input('Zadej zprávu: ')
-else:
-    print('Zadej číslo ve správném formátu!')
 
 def spocitej_cenu(text):
     if len(text) <= 180:
@@ -33,6 +12,9 @@ def spocitej_cenu(text):
     radky = ((len(text) // 180) + 1) 
     return radky * 3
 
-cena_zpravy = spocitej_cenu(text)
-
-print(f'Cena zprávy je {cena_zpravy} Kč.')
+if over_delku(zadane_cislo):
+    zadany_text = input('Zadej zprávu: ')
+    cena_zpravy = spocitej_cenu(zadany_text)
+    print(f'Cena zprávy je {cena_zpravy} Kč.')
+else:
+    print('Zadej číslo ve správném formátu! Číslo musí být devítimístné nebo třináctimistné s českou předvolbou +420.')
