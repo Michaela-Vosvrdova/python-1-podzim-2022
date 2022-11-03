94da459256e0505da6c6589b7c22debc8d9e9ed3
Michaela Vosvrdova <vosv@protonmail.com>
2022-11-03 21:12:01 +0100

    Finální verze

--- ukol-06.py
+++ ukol-06.py
@@ -1,6 +1,11 @@
-with open('muj_vykaz.txt', 'r', encoding='utf-8') as soubor:
-    radky = soubor.readlines()
+with open('vykaz.txt', 'r', encoding='utf-8') as vstup:
+    radky = vstup.readlines()
 
-new_dict = {'1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E'}
-radky = [radek.replace('\t', ' ').replace('\n', '').replace(new_dict.keys(), new_dict.values()) for radek in radky]
-print(radky)
+prvni_radek = radky[0]
+prevod_znamek = {ord('1'): 'A', ord('2'): 'B', ord('3'): 'C', ord('4'): 'D', ord('5'): 'E'}
+radky = [radek.replace('\n', '').translate(prevod_znamek) for radek in radky[1:]]
+
+with open('novy_vykaz.txt', 'w', encoding='utf-8') as vystup:
+    vystup.write(prvni_radek)
+    for radek in radky:
+        vystup.write(radek + '\n')
